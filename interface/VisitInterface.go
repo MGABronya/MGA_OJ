ce7d46b52ef3f07f93b4c8c015aafbb6b5fde332
MGAronya <20jzhang@stu.edu.cn>
2022-11-24 16:09:58 +0800

    游览方法接口

---
+++ interface/VisitInterface.go
@@ -0,0 +1,15 @@
+// @Title  VisitInterface
+// @Description  该文件用于封装游览方法
+// @Author  MGAronya（张健）
+// @Update  MGAronya（张健）  2022-9-16 0:33
+package Interface
+
+import "github.com/gin-gonic/gin"
+
+// VisitInterface			定义了游览方法
+type VisitInterface interface {
+	Visit(ctx *gin.Context)       // 游览
+	VisitNumber(ctx *gin.Context) // 游览人数
+	VisitList(ctx *gin.Context)   // 游览列表
+	Visits(ctx *gin.Context)      // 用户游览历史记录
+} 
