2320828451bb4461330675bb493fe56b657c0f8f
MGAronya <20jzhang@stu.edu.cn>
2022-11-24 16:09:50 +0800

    增删查改接口

--- interface/RestInterface.go
+++ interface/RestInterface.go
@@ -8,9 +8,9 @@ import "github.com/gin-gonic/gin"
 
 // RestInterface			定义了增删查改方法
 type RestInterface interface {
-	Create(ctx *gin.Context)   //增
-	Update(ctx *gin.Context)   //删
-	Show(ctx *gin.Context)     //查
-	Delete(ctx *gin.Context)   //改
+	Create(ctx *gin.Context)   // 增
+	Update(ctx *gin.Context)   // 删
+	Show(ctx *gin.Context)     // 查
+	Delete(ctx *gin.Context)   // 改
 	PageList(ctx *gin.Context) // 查看列表
 }
