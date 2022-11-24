f167e0d472bd6f0896935990f3be1d5f6e25f524
MGAronya <20jzhang@stu.edu.cn>
2022-11-24 16:08:21 +0800

    评论控制器

--- controller/ReplyController.go
+++ controller/ReplyController.go
@@ -19,7 +19,9 @@ import (
 
 // IReplyController			定义了讨论的回复类接口
 type IReplyController interface {
-	Interface.RestInterface // 包含增删查改功能
+	Interface.RestInterface    // 包含增删查改功能
+	Interface.LikeInterface    // 包含点赞功能
+	UserList(ctx *gin.Context) // 查看指定用户的回复
 }
 
 // ReplyController			定义了讨论的回复工具类
@@ -178,17 +180,250 @@ func (r ReplyController) PageList(ctx *gin.Context) {
 	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
 	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
 
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
 	// TODO 分页
 	var replys []model.Reply
 
 	// TODO 查找所有分页中可见的条目
-	r.DB.Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&replys)
+	r.DB.Where("comment_id = ?", id).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&replys)
 
 	var total int64
-	r.DB.Model(model.Reply{}).Count(&total)
+	r.DB.Where("comment_id = ?", id).Model(model.Reply{}).Count(&total)
 
 	// TODO 返回数据
 	response.Success(ctx, gin.H{"replys": replys, "total": total}, "成功")
+}
+
+// @title    UserList
+// @description   获取多篇指定用户讨论的回复
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) UserList(ctx *gin.Context) {
+	// TODO 获取分页参数
+	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
+	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
+
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 分页
+	var replys []model.Reply
+
+	// TODO 查找所有分页中可见的条目
+	r.DB.Where("user_id = ?", id).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&replys)
+
+	var total int64
+	r.DB.Where("user_id = ?", id).Model(model.Reply{}).Count(&total)
+
+	// TODO 返回数据
+	response.Success(ctx, gin.H{"replys": replys, "total": total}, "成功")
+}
+
+// @title    Like
+// @description   点赞或点踩
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) Like(ctx *gin.Context) {
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	var reply model.Reply
+
+	// TODO 查看讨论是否存在
+	if r.DB.Where("id = ?", id).First(&reply).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取登录用户
+	tuser, _ := ctx.Get("user")
+	user := tuser.(model.User)
+
+	// TODO 如果没有点赞或者点踩
+	if r.DB.Where("user_id = ? and reply_id = ?", user.ID, id).Update("like", like).Error != nil {
+		// TODO 插入数据
+		replyLike := model.ReplyLike{
+			ReplyId: reply.ID,
+			UserId:  user.ID,
+			Like:    like,
+		}
+		if err := r.DB.Create(&replyLike).Error; err != nil {
+			response.Fail(ctx, nil, "点赞出错，数据库存储错误")
+			return
+		}
+	}
+
+	response.Success(ctx, nil, "点赞成功")
+}
+
+// @title    CancelLike
+// @description   取消点赞或者点踩
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) CancelLike(ctx *gin.Context) {
+	// 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	var reply model.Reply
+
+	// TODO 查看题目是否存在
+	if r.DB.Where("id = ?", id).First(&reply).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取登录用户
+	tuser, _ := ctx.Get("user")
+	user := tuser.(model.User)
+
+	// TODO 取消点赞或者点踩
+	r.DB.Where("user_id = ? and reply_id = ?", user.ID, id).Delete(&model.ReplyLike{})
+
+	response.Success(ctx, nil, "取消成功")
+}
+
+// @title    LikeNumber
+// @description   点赞或点踩的数量
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) LikeNumber(ctx *gin.Context) {
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	var reply model.Reply
+
+	// TODO 查看题目是否存在
+	if r.DB.Where("id = ?", id).First(&reply).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	var total int64
+
+	// TODO 查看点赞或者点踩的数量
+	r.DB.Where("reply_id = ? and like = ?", id, like).Count(&total)
+
+	response.Success(ctx, gin.H{"total": total}, "查看成功")
+}
+
+// @title    LikeList
+// @description   点赞或点踩的列表
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) LikeList(ctx *gin.Context) {
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	var reply model.Reply
+
+	// TODO 查看题目是否存在
+	if r.DB.Where("id = ?", id).First(&reply).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取分页参数
+	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
+	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
+
+	// TODO 分页
+	var replyLikes []model.ReplyLike
+
+	var total int64
+
+	// TODO 查看点赞或者点踩的数量
+	r.DB.Where("reply_id = ? and like = ?", id, like).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&replyLikes).Count(&total)
+
+	response.Success(ctx, gin.H{"replyLikes": replyLikes, "total": total}, "查看成功")
+}
+
+// @title    LikeShow
+// @description   查看用户点赞状态
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) LikeShow(ctx *gin.Context) {
+	// 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	var reply model.Reply
+
+	// TODO 查看讨论是否存在
+	if r.DB.Where("id = ?", id).First(&reply).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取登录用户
+	tuser, _ := ctx.Get("user")
+	user := tuser.(model.User)
+
+	var replyLike model.ReplyLike
+
+	// TODO 查看点赞状态
+	if r.DB.Where("user_id = ? and reply_id = ?", user.ID, id).First(&replyLike).Error != nil {
+		response.Success(ctx, gin.H{"like": 0}, "暂无状态")
+		return
+	}
+
+	if replyLike.Like {
+		response.Success(ctx, gin.H{"like": 1}, "已点赞")
+	} else {
+		response.Success(ctx, gin.H{"like": -1}, "已点踩")
+	}
+
+}
+
+// @title    Likes
+// @description   查看用户点赞状态
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (r ReplyController) Likes(ctx *gin.Context) {
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	// TODO 获取分页参数
+	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
+	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
+
+	/// TODO 获取指定用户用户
+	id := ctx.Params.ByName("id")
+
+	var user model.User
+
+	// TODO 查看用户是否存在
+	if r.DB.Where("id = ?", id).First(&user).Error != nil {
+		response.Fail(ctx, nil, "用户不存在")
+		return
+	}
+
+	// TODO 分页
+	var replyLikes []model.ReplyLike
+
+	var total int64
+
+	// TODO 查看点赞或者点踩的数量
+	r.DB.Where("user_id = ? and like = ?", user.ID, like).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&replyLikes).Count(&total)
+
+	response.Success(ctx, gin.H{"replyLikes": replyLikes, "total": total}, "查看成功")
 }
 
 // @title    NewReplyController
@@ -199,5 +434,6 @@ func (r ReplyController) PageList(ctx *gin.Context) {
 func NewReplyController() IReplyController {
 	db := common.GetDB()
 	db.AutoMigrate(model.Reply{})
+	db.AutoMigrate(model.ReplyLike{})
 	return ReplyController{DB: db}
 }
