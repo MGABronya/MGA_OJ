e8d7559ee63b70f367444f4a77dd96e41c63bf3a
MGAronya <20jzhang@stu.edu.cn>
2022-11-24 16:08:34 +0800

    题解回复控制器

--- controller/ThreadController.go
+++ controller/ThreadController.go
@@ -19,7 +19,9 @@ import (
 
 // IThreadController			定义了题解的回复类接口
 type IThreadController interface {
-	Interface.RestInterface // 包含增删查改功能
+	Interface.RestInterface    // 包含增删查改功能
+	Interface.LikeInterface    // 包含点赞功能
+	UserList(ctx *gin.Context) // 指定用户的题解回复
 }
 
 // ThreadController			定义了题解的回复工具类
@@ -178,17 +180,250 @@ func (t ThreadController) PageList(ctx *gin.Context) {
 	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
 	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
 
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
 	// TODO 分页
 	var threads []model.Thread
 
 	// TODO 查找所有分页中可见的条目
-	t.DB.Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&threads)
+	t.DB.Where("post_id = ?", id).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&threads)
 
 	var total int64
-	t.DB.Model(model.Thread{}).Count(&total)
+	t.DB.Where("post_id = ?", id).Model(model.Thread{}).Count(&total)
 
 	// TODO 返回数据
 	response.Success(ctx, gin.H{"threads": threads, "total": total}, "成功")
+}
+
+// @title    UserList
+// @description   获取多篇指定用户的题解的回复
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) UserList(ctx *gin.Context) {
+	// TODO 获取分页参数
+	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
+	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
+
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 分页
+	var threads []model.Thread
+
+	// TODO 查找所有分页中可见的条目
+	t.DB.Where("user_id = ?", id).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&threads)
+
+	var total int64
+	t.DB.Where("user_id = ?", id).Model(model.Thread{}).Count(&total)
+
+	// TODO 返回数据
+	response.Success(ctx, gin.H{"threads": threads, "total": total}, "成功")
+}
+
+// @title    Like
+// @description   点赞或点踩
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) Like(ctx *gin.Context) {
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	var thread model.Thread
+
+	// TODO 查看讨论是否存在
+	if t.DB.Where("id = ?", id).First(&thread).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取登录用户
+	tuser, _ := ctx.Get("user")
+	user := tuser.(model.User)
+
+	// TODO 如果没有点赞或者点踩
+	if t.DB.Where("user_id = ? and thread_id = ?", user.ID, id).Update("like", like).Error != nil {
+		// TODO 插入数据
+		threadLike := model.ThreadLike{
+			ThreadId: thread.ID,
+			UserId:   user.ID,
+			Like:     like,
+		}
+		if err := t.DB.Create(&threadLike).Error; err != nil {
+			response.Fail(ctx, nil, "点赞出错，数据库存储错误")
+			return
+		}
+	}
+
+	response.Success(ctx, nil, "点赞成功")
+}
+
+// @title    CancelLike
+// @description   取消点赞或者点踩
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) CancelLike(ctx *gin.Context) {
+	// 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	var thread model.Thread
+
+	// TODO 查看题目是否存在
+	if t.DB.Where("id = ?", id).First(&thread).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取登录用户
+	tuser, _ := ctx.Get("user")
+	user := tuser.(model.User)
+
+	// TODO 取消点赞或者点踩
+	t.DB.Where("user_id = ? and thread_id = ?", user.ID, id).Delete(&model.ThreadLike{})
+
+	response.Success(ctx, nil, "取消成功")
+}
+
+// @title    LikeNumber
+// @description   点赞或点踩的数量
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) LikeNumber(ctx *gin.Context) {
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	var thread model.Thread
+
+	// TODO 查看题目是否存在
+	if t.DB.Where("id = ?", id).First(&thread).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	var total int64
+
+	// TODO 查看点赞或者点踩的数量
+	t.DB.Where("thread_id = ? and like = ?", id, like).Count(&total)
+
+	response.Success(ctx, gin.H{"total": total}, "查看成功")
+}
+
+// @title    LikeList
+// @description   点赞或点踩的列表
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) LikeList(ctx *gin.Context) {
+	// TODO 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	var thread model.Thread
+
+	// TODO 查看题目是否存在
+	if t.DB.Where("id = ?", id).First(&thread).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取分页参数
+	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
+	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
+
+	// TODO 分页
+	var threadLikes []model.ThreadLike
+
+	var total int64
+
+	// TODO 查看点赞或者点踩的数量
+	t.DB.Where("thread_id = ? and like = ?", id, like).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&threadLikes).Count(&total)
+
+	response.Success(ctx, gin.H{"threadLikes": threadLikes, "total": total}, "查看成功")
+}
+
+// @title    LikeShow
+// @description   查看用户点赞状态
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) LikeShow(ctx *gin.Context) {
+	// 获取path中的id
+	id := ctx.Params.ByName("id")
+
+	var thread model.Thread
+
+	// TODO 查看讨论是否存在
+	if t.DB.Where("id = ?", id).First(&thread).Error != nil {
+		response.Fail(ctx, nil, "讨论不存在")
+		return
+	}
+
+	// TODO 获取登录用户
+	tuser, _ := ctx.Get("user")
+	user := tuser.(model.User)
+
+	var threadLike model.ThreadLike
+
+	// TODO 查看点赞状态
+	if t.DB.Where("user_id = ? and thread_id = ?", user.ID, id).First(&threadLike).Error != nil {
+		response.Success(ctx, gin.H{"like": 0}, "暂无状态")
+		return
+	}
+
+	if threadLike.Like {
+		response.Success(ctx, gin.H{"like": 1}, "已点赞")
+	} else {
+		response.Success(ctx, gin.H{"like": -1}, "已点踩")
+	}
+
+}
+
+// @title    Likes
+// @description   查看用户点赞状态
+// @auth      MGAronya（张健）       2022-9-16 12:20
+// @param    ctx *gin.Context       接收一个上下文
+// @return   void
+func (t ThreadController) Likes(ctx *gin.Context) {
+
+	// TODO 获取like
+	like, _ := strconv.ParseBool(ctx.Query("like"))
+
+	// TODO 获取分页参数
+	pageNum, _ := strconv.Atoi(ctx.DefaultQuery("pageNum", "1"))
+	pageSize, _ := strconv.Atoi(ctx.DefaultQuery("pageSize", "20"))
+
+	// TODO 获取指定用户用户
+	id := ctx.Params.ByName("id")
+
+	var user model.User
+
+	// TODO 查看用户是否存在
+	if t.DB.Where("id = ?", id).First(&user).Error != nil {
+		response.Fail(ctx, nil, "用户不存在")
+		return
+	}
+
+	// TODO 分页
+	var threadLikes []model.ThreadLike
+
+	var total int64
+
+	// TODO 查看点赞或者点踩的数量
+	t.DB.Where("user_id = ? and like = ?", user.ID, like).Order("created_at desc").Offset((pageNum - 1) * pageSize).Limit(pageSize).Find(&threadLikes).Count(&total)
+
+	response.Success(ctx, gin.H{"threadLikes": threadLikes, "total": total}, "查看成功")
 }
 
 // @title    NewThreadController
@@ -199,5 +434,6 @@ func (t ThreadController) PageList(ctx *gin.Context) {
 func NewThreadController() IThreadController {
 	db := common.GetDB()
 	db.AutoMigrate(model.Thread{})
+	db.AutoMigrate(model.ThreadLike{})
 	return ThreadController{DB: db}
 }
